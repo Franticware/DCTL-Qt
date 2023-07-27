7daa3c86a30000271da3368d910bd6c6cb094bc2
Vojta <vojta.salajka@seznam.cz>
2023-07-27 23:06:06 +0200

    More complete set of math functions

--- dctl_parta.h
+++ dctl_parta.h
@@ -52,85 +52,177 @@ struct Texture
 
 typedef const Texture& __TEXTURE__;
 
-inline float _acosf(float f)
+inline float _fabs(float x) // Returns the absolute value of x
 {
-    return std::acos(f);
+    return std::abs(x);
 }
-
-inline float _clampf(float f, float l, float h)
+inline float _powf(float x, float y) // Computes x raised to the power of y
 {
-    return std::clamp(f, l, h);
+    return std::pow(x, y);
 }
-
-inline float _copysignf(float x, float y)
+inline float _logf(float x) // Computes the value of the natural logarithm of x
+{
+    return std::log(x);
+}
+inline float _log2f(float x) // Computes the value of the logarithm of x to base 2
+{
+    return std::log2(x)    ;
+}
+inline float _log10f(float x) // Computes the value of the logarithm of x to base 10
+{
+    return std::log10(x);
+}
+inline float _expf(float x) // Computes e**x, the base-e exponential of x
+{
+    return std::exp(x);
+}
+inline float _exp2f(float x) // Computes 2**x, the base-2 exponential of x
+{
+    return std::exp2(x);
+}
+inline float _exp10f(float x) // Computes 10**x, the base-10 exponential of x
+{
+    return std::pow(10.f, x);
+}
+inline float _copysignf(float x, float y) // Returns x with its sign changed to y’s
 {
     return std::copysign(x, y);
 }
-
-inline float _cosf(float f)
+inline float _fmaxf(float x, float y) // Returns x or y, whichever is larger
 {
-    return std::cos(f);
+    return std::max(x, y);
 }
-
-inline float _exp2f(float f)
+inline float _fminf(float x, float y) // Returns x or y, whichever is smaller
 {
-    return std::exp2(f);
+    return std::min(x, y);
 }
-
-inline float _fabs(float f)
+inline float _clampf(float x, float min, float max) // Clamps x to be within the interval [min, max]
 {
-    return std::fabs(f);
+    return std::clamp(x, min, max);
 }
-
-inline float _fdivide(float x, float y)
+inline float _saturatef(float x) // Clamps x to be within the interval [0.0f, 1.0f]
 {
-    return x/y;
+    return std::clamp(x, 0.f, 1.f);
 }
-
-inline float _floor(float f)
+inline float _sqrtf(float x) // Computes the non-negative square root of x
 {
-    return std::floor(f);
+    return std::sqrt(x);
 }
-
-inline float _fmaf(float x, float y, float z)
+inline float _ceilf(float x) // Returns the smallest integral value greater than or equal to x
 {
-    return std::fma(x, y, z);
+    return std::ceil(x);
 }
-
-inline float _fmaxf(float x, float y)
+inline float _ceil(float x)
 {
-    return std::fmax(x, y);
+    return std::ceil(x);
 }
-
-inline float _fminf(float x, float y)
+inline float _floorf(float x) // Returns the largest integral value less than or equal to x
 {
-    return std::fmin(x, y);
+    return std::floor(x);
 }
-
-inline float _hypotf(float x, float y)
+inline float _floor(float x)
+{
+    return std::floor(x);
+}
+inline float _truncf(float x) // Returns the integral value nearest to but no larger in magnitude than x
+{
+    return std::trunc(x);
+}
+inline float _round(float x) // Returns the integral value nearest to x rounding, with half-way cases rounded away from zero
+{
+    return std::round(x);
+}
+inline float _fmod(float x, float y) // Computes the floating-point remainder of x/y
+{
+    return std::fmod(x, y);
+}
+inline float _hypotf(float x, float y) // Computes the square root of the sum of squares of x and y
 {
     return std::hypot(x, y);
 }
-
-inline float _round(float f)
+inline float _cosf(float x) // Computes the cosine of x (measured in radians)
 {
-    return std::round(f);
+    return std::cos(x);
 }
-
-inline float _saturatef(float f)
+inline float _sinf(float x) // Computes the sine of x (measured in radians)
 {
-    if (f != f) return 0;
-    return _clampf(f, 0, 1);
+    return std::sin(x);
 }
-
-inline float _sinf(float f)
+inline float _cospif(float x) // Computes the cosine of (x * pi) (measured in radians)
 {
-    return std::sin(f);
+    return std::cos(x * M_PIf);
 }
-
-inline float _tanf(float f)
+inline float _sinpif(float x) // Computes the sine of (x * pi) (measured in radians)
 {
-    return std::tan(f);
+    return std::sin(x * M_PIf);
+}
+inline float _tanf(float x) // Computes the tangent of x (measured in radians)
+{
+    return std::tan(x);
+}
+inline float _acosf(float x) // Computes the principal value of the arc cosine of x
+{
+    return std::acos(x);
+}
+inline float _asinf(float x) // Computes the principal value of the arc sine of x
+{
+    return std::asin(x);
+}
+inline float _atan2f(float y, float x) // Computes the principal value of the arc tangent of y/x, using the signs of both arguments to determine the quadrant of the return value
+{
+    return std::atan2(y, x);
+}
+inline float _acoshf(float x) // Computes the principal value of the inverse hyperbolic cosine of x
+{
+    return std::acosh(x);
+}
+inline float _asinhf(float x) // Computes the principal value of the inverse hyperbolic sine of x
+{
+    return std::asinh(x);
+}
+inline float _atanhf(float x) // Computes the inverse hyperbolic tangent of x
+{
+    return std::atanh(x);
+}
+inline float _coshf(float x) // Computes the hyperbolic cosine of x
+{
+    return std::cosh(x);
+}
+inline float _sinhf(float x) // Computes the hyperbolic sine of x
+{
+    return std::sinh(x);
+}
+inline float _tanhf(float x) // Computes the hyperbolic tangent of x
+{
+    return std::tanh(x);
+}
+inline float _fdimf(float x, float y) // Returns the positive difference between x and y:x - y if x > y, +0 if x is less than or equal to y
+{
+    return std::fdim(x, y);
+}
+inline float _fmaf(float x, float y, float z) // Computes (x * y) + z as a single operation
+{
+    return (x * y) + z;
+}
+inline float _rsqrtf(float x) // Computes the reciprocal of square root of x
+{
+    return 1.f/std::sqrt(x);
+}
+inline float _fdivide(float x, float y) // Returns x/y
+{
+    return x/y;
+}
+inline float _frecip(float x) // Returns 1/x
+{
+    return 1.f/x;
+}
+inline int isinf(float x) // Returns a non-zero value if and only if x is an infinite value
+{
+    return std::isinf(x) ? 1 : 0;
+}
+inline int isnan(float x) // Returns a non-zero value if and only if x is a NaN value
+{
+    return std::isnan(x) ? 1 : 0;
 }
 
 inline float _tex2D(__TEXTURE__ t, float xf, float yf)
